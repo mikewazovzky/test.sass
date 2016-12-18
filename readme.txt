e815167b1312b41ef16ee1601726cd25c72ef237
Mike Wazovzky <mike.wazovzky@gmail.com>
2016-12-18 11:21:02 +0300

    sass introduction: syntax, variables, mixins, file/catalog structure

---
+++ readme.txt
@@ -0,0 +1,7 @@
+Learn SASS
+Environment: Laraval Elixir/Gulp (gulp-sass) as preproccessor
+
+Topics:
+1. SASS file structure
+2. Variables
+3. Mixins
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,7 +17,97 @@
         <div class="container">
             <h1>About Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button class="btn btn-primary">Press Me</button>
+            <h1>Sections</h1>
+            <ul>
+            
+                <li><a href="#introduction">Introduction</a></li>
+                <li><a href="#mesa">Mesa</a></li> 
+                <li><a href="#buttons">Buttons</a></li>              
+                <li><a href="#team">Team</a></li>
+                <li><a href="#jumbotron">Jumbotron</a></li>
+            </ul>
+            
+            <section id="introduction">
+                <div class="container">
+                    <h1>Introduction</h1>              
+                    <ol class="team">
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/sulley.jpg">
+                                <h2 class="head">Sulley</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/boo.jpg">
+                                <h2 class="head">Boo</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>                        
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/mw.jpg">
+                                <h2 class="head">Mike</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>               
+                    </ol>         
+                </div>
+            </section>  
+            
+            <section class="mesa background" id="mesa">
+                <div class="container">
+                    <h1>Mesa Arch</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                </div>
+            </section>  
+            
+            <section id="buttons">    
+                <div class="container">
+                    <button class="btn-default">Default Button</button>  
+                    <button class="btn-cool">Cool</button> 
+                    <button class="btn-hot">Hot</button>               
+                </div>
+            </section>  
+            
+            <section id="team">
+                <div class ="container">
+                    <h1>Team</h1>
+                    <ol class="team">
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/sulley.jpg">
+                                <h2 class="head">Sulley</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/boo.jpg">
+                                <h2 class="head">Boo</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>                        
+                        <li class="item">
+                            <a href="">
+                                <img src="/images/mw.jpg">
+                                <h2 class="head">Mike</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                            </a>
+                        </li>                        
+                        
+                    </ol>
+                </div>
+            </section>
+            
+            <br>
+            <section class="jumbotron background" id="jumbotron">
+                <div class="container">
+                    <h1>Jumbotron</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                </div>
+            </section>
         </div>
     </body>
 </html>
